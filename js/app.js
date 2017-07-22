67dbfd913083718750685042cf7115001e54d0c6
Puyih Severino <yihseverino@gmail.com>
2017-07-22 21:29:38 +0800

    minor adjustments
    adjust offset to view title of section
    change text style when on focus
    added experience to navbar

--- js/app.js
+++ js/app.js
@@ -82,7 +82,7 @@ $(document).ready(function () {
     $(".scroll").click(function(event) {
         event.preventDefault();
         $('html,body').animate({
-            scrollTop: $(this.hash).offset().top
+            scrollTop: $(this.hash).offset().top -30
         }, 1000);
     });
 
