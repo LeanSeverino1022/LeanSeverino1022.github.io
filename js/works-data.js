9d2ac436e88dd7cec93abf7633145ae6c5a87fe9
Puyih Severino <yihseverino@gmail.com>
2019-01-29 17:18:38 +0800

    debugging file(image) not found error

--- js/works-data.js
+++ js/works-data.js
@@ -25,7 +25,7 @@
             $(anchor).attr({ href: 'https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_link_image' });
 
             let img = document.createElement("img");
-            $(img).attr({ class: 'img-responsive' , alt:'github-ss', src: 'images/Capture.png'});
+            $(img).attr({ class: 'img-responsive' , alt:'github-ss', src: 'icons/git.svg'});
 
             // return card. 1 card = 1 project
             return $(div).append($(anchor).append(img)) 
